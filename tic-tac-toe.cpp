8d53a675d00ebb19b63a1f37cff2e38843c38303
RedCrabVb <61832494+RedCrabVb@users.noreply.github.com>
2020-09-04 18:55:57 +0300

    Update tic-tac-toe.cpp

--- tic-tac-toe.cpp
+++ tic-tac-toe.cpp
@@ -1,196 +1,128 @@
+//version 0.1
+//version 0.2
+/*Добавлена графическая состовляющия; Вызов не нужного метода*/
 #include <iostream>
-#include <ctime>
+#include <string>
+#include <math.h>
+
 #include <stdio.h>
 #include <unistd.h>
 #include <termios.h>
 
 using namespace std;
 
-char arr_tabl[9] = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
-char I, ai, vis;// см standart_setengs
-int g;//см standart_setengs
-bool game = true;
+int n, m;
+long res;
+char opt;
 
-struct termios old_tio, new_tio;//для каретной работы клавиш
-unsigned char con; //con - control
+string yes_no = "\x1b[0;30;47my(да)/n(нет): \x1b[0m";
+string error = "Выйди и зайди нармально! некркетный ввод\n\n";
 
-void show_tabl(){
-        system("clear");
-        for(int j = 1; j <= 1; j++){
-                printf("\x1b[7;37m\n %c | %c | %c \n - | - | - \x1b[0m", arr_tabl[j-1], arr_tabl[j], arr_tabl[j+1]);
-                printf("\x1b[7;37m\n %c | %c | %c \n - | - | - \x1b[0m", arr_tabl[j+2], arr_tabl[j+3], arr_tabl[j+4]);
-                printf("\x1b[7;37m\n %c | %c | %c \x1b[0m\n", arr_tabl[j+5], arr_tabl[j+6], arr_tabl[j+7]);
-                printf("\n");
-        }
+long double fact(int N)
+{
+    if (N == 0)
+        return 1;
+    else
+        return N * fact(N - 1); // делаем рекурсию.
+}
+//bag??
+void repeat_all_no(){
+  opt == ' ';
+  cout<<"Есть повтор? " << yes_no<<endl;
+  cin>>opt;
+  if(opt == 'y' || opt == 'Y'){
+    //A(-(m_n)=n^m;
+    cout<<"A(-(m_n)=n^m"<<endl;
+    res = pow(n,m);
+  }
+  else if(opt == 'n' || opt == 'N'){
+    //A=n!(n-m)!
+    cout<<"A=n!(n-m)!"<<endl;
+    res = fact(n)/fact(n-m);
+  }
+  else{
+    cout<<error<<endl;
+  }
 }
 
-//ничья
-bool check_endBad(){
-
-       bool eb = false;
-       for(int i = 0; i < 9; i++){
-            if(arr_tabl[i] == ' '){eb = true;}
-            else if(arr_tabl[i] == '+'){cout<<"bag\a";}
-        }
-        if(game == false){
-                eb = false;
-        }
-        else if(game == true && eb == false){
-                show_tabl();
-                eb == false;
-                cout<<"ничья!\n";
-        }
-        return eb;
+//Bug!!
+void repeat_all_yes(){
+  opt == ' ';
+  cout<<"Есть повтор? "<< yes_no <<endl;
+  cin>>opt;
+  if(opt == 'y' || opt == 'Y'){
+    cout<<"P(n)=n!/(n1!*n2!...nm!)"<<endl;
+    //P(n)=n!/(n1!*n2!...nm!)
+    res = fact(n)/fact(9);                    //Problev!!
+  }
+  else if(opt == 'n' || opt == 'N'){
+    cout<<"P(n)=n!"<<endl;
+    res = fact(n);
+    //P(n)=n!
+  }
+  else{
+    cout<<error<<endl;
+  }
 }
 
-//проверка на победу
-void win(){
-        char win = ' ';
-  //---
-        if(arr_tabl[0] == arr_tabl[1] && arr_tabl[1] == arr_tabl[2] && arr_tabl[0] != ' '){
-                win = arr_tabl[0];
-                game = false;
-        }
-        else if(arr_tabl[3] == arr_tabl[4] && arr_tabl[4] == arr_tabl[5] && arr_tabl[3] != ' '){
-                win = arr_tabl[3];
-                game = false;
-              }
-        else  if(arr_tabl[6] == arr_tabl[7] && arr_tabl[7] == arr_tabl[8] && arr_tabl[8] != ' '){
-                win = arr_tabl[6];
-                game = false;
-        }
-  //|||
-        else if(arr_tabl[0] == arr_tabl[3] && arr_tabl[3] == arr_tabl[6] &&  arr_tabl[6] != ' '){
-                win = arr_tabl[0];
-                game = false;
-        }
-        else if(arr_tabl[1] == arr_tabl[4] && arr_tabl[4] == arr_tabl[7] && arr_tabl[1] != ' '){
-                win = arr_tabl[1];
-                game = false;
-        }
-        else if(arr_tabl[2] == arr_tabl[5] && arr_tabl[5] == arr_tabl[8] && arr_tabl[5] != ' '){
-                win = arr_tabl[2];
-                game = false;
-        }
-  //\/
-        else if(arr_tabl[0] == arr_tabl[4] && arr_tabl[4] == arr_tabl[8] && arr_tabl[4] != ' '){
-                win = arr_tabl[0];
-                game = false;
-        }
-        else if(arr_tabl[2] == arr_tabl[4] && arr_tabl[4] == arr_tabl[6] && arr_tabl[4] != ' '){
-                win = arr_tabl[2];
-                game = false;
-        }
-        else{
-                game = check_endBad();
-        }
-
-        if(win != ' '){
-          show_tabl();
-          printf("\x1b[7;34mПобедил  %c!\x1b[0m\n", win);
-        }
+void slect_all(){
+  opt == ' ';
+  cout<<"Нужноли выбрать все элементы? " << yes_no <<endl;
+  cin>>opt;
+  if(opt == 'y' || opt == 'Y'){
+    repeat_all_yes();
+  }
+  else if(opt == 'n' || opt == 'N'){
+    repeat_all_no();
+  }
+  else{
+    cout<<error<<endl;
+  }
 }
 
-//стандартное управление wasd, запись r + не требуется нажатие enter решение подсмотрел
-int control(int i, char swapp){
-        swapp = arr_tabl[i];
-        arr_tabl[i] = vis;
-        show_tabl();
-
-        con=getchar();
-//      cin>>con;
-        switch(con){
-                case 'd':
-                        arr_tabl[i] = swapp;
-                        i++;
-                        if((i >= 9) || (i < 0))
-                                 i--;
-                        break;
-                case 'a':
-                        arr_tabl[i] = swapp;
-                        i--;
-                        if((i >= 9) || (i < 0))
-                                 i++;
-                        break;
-                case 'r':
-                        if(swapp == ' '){
-                                arr_tabl[i] = I;
-                                show_tabl();
-                                cout<<"\a";
-                                win();
-                                return 1;
-
-                        }
-                else{
-                                cout<<"\x1b[5;37mNo\n\a\x1b[0m";
-                                arr_tabl[i] = swapp;
-                                i = 0;
-                                system("sleep 0.9");
-                        }
-                        break;
-                case 's':
-                        arr_tabl[i] = swapp;
-                        i+=3;
-                        if(i >= 9)
-                                 i-=3;
-                        break;
-                case 'w':
-                        arr_tabl[i] = swapp;
-                        i-=3;
-                        if(i<0)
-                                i+=3;
-                        break;
-                default:
-                        arr_tabl[i] = swapp;
-                        i = 0;
-                        system("sleep 0.3");
-                        break;
-        }
-        control(i, swapp);
-        return 0;
+//Bug?
+void repeat(){
+  opt == ' ';
+  cout<<"Есть повтор? "<< yes_no <<endl;
+  cin>>opt;
+  if(opt == 'y' || opt == 'Y'){
+    cout<<"c(-m_n)=(n+m-1)!/(m!(n-1))"<<endl;
+    res = fact((n+m-1))/(fact(m)*fact((n-1)));
+  }
+  else if(opt == 'n' || opt == 'N'){
+    cout<<"c(m_n)=n!/(m!(n-m)!)"<<endl;
+    res = fact(n)/(fact(m)*fact((n-m)));
+  }
+  else{
+    cout<<error<<endl;
+  }
 }
 
-//симулирует играка
-void ai_v(){
-        bool k = check_endBad();
-        while(k){
-                int ran = rand() % 8;
-                if((arr_tabl[ran] != I) && (arr_tabl[ran] != ai)){
-                        arr_tabl[ran] = ai;
-                        k = false;
-                        win();
-                        }
-        }
-}
-
-void standart_setengs(){
-        vis = '@';
-        g = 0;//стандартная позиция
-        cout<<"\x1b[5;32mКонтроль: a, w, s, d - перемещение r-запесь\x1b[0m";
-        cout<<"\n\vКого выберете? 0 или X или ...? ";
-        cin>>I;
-        if(I == 'X' || I == 'x')
-                ai = '0';
-        else
-                ai = 'X';
+void order(){
+  opt == ' ';
+  cout<<"Важен порядок? "<< yes_no <<endl;
+  system("sleep 2");
+  cin>>opt;
+  if(opt == 'y' || opt == 'Y'){
+      slect_all();
+  }
+  else if(opt == 'n' || opt == 'N'){
+      repeat();
+  }
+  else{
+    cout<<error<<endl;
+  }
 }
 
 int main(){
-        //для клавиш
-        tcgetattr(STDIN_FILENO,&old_tio);
-        new_tio=old_tio;
-        new_tio.c_lflag &=(~ICANON & ~ECHO);
-        tcsetattr(STDIN_FILENO,TCSANOW,&new_tio);
-
-        standart_setengs();
-        srand(time(0));
-        while(game){
-                show_tabl();
-                control(g, arr_tabl[g]);
-                system("sleep 0.3");
-                ai_v();
-        }
-        //Ввозвращаем клавиатуру
-        tcsetattr(STDIN_FILENO,TCSANOW,&old_tio);
-        return 0;
+  system("clear");
+  cout<<"Алгоритм:"<<endl;
+  cout<<"\x1b[4;31mn - общие кал-во элементо: \x1b[0m"<<endl;
+  cin>>n;
+  cout<<"\x1b[4;32mm - сколько эл.в выбора: \x1b[0m"<<endl;
+  cin>>m;
+  order();
+  //Ввозвращаем клавиатуру
+  cout<<"Результат: "<<res<<endl;
+  return 0;
 }
